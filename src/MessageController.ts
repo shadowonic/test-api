53c387fb0f09808aaadbde26386b8c7158203f9c
hike-zzz <y.plastun@gmail.com>
2018-02-13 22:17:17 +0200

    Done

--- src/MessageController.ts
+++ src/MessageController.ts
@@ -1,9 +1,10 @@
-import {OnConnect, SocketController, ConnectedSocket, OnDisconnect, MessageBody, OnMessage} from 'socket-controllers';
+import { OnConnect, SocketController, ConnectedSocket, OnDisconnect, MessageBody, OnMessage } from 'socket-controllers';
 
 @SocketController()
 export class MessageController {
   @OnConnect()
   public connection(@ConnectedSocket() socket: any) {
+    socket.handshake.user = { id: 1 };
     console.info('client connected');
   }
 
@@ -17,6 +18,6 @@ export class MessageController {
     console.info('received message:', message);
     console.info('setting id to the message and sending it back to the client');
     message.id = 1;
-    socket.emit('message', {type: 'message_saved', message});
+    socket.emit('message', { type: 'message_saved', message });
   }
 }
